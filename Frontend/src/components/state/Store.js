c2cde52faccf64c987f575c2712ffbf3a70a353c
MILIND SINGH <milluk813@gmail.com>
2025-03-23 19:41:22 +0530

    integration

--- Frontend/src/components/state/Store.js
+++ Frontend/src/components/state/Store.js
@@ -44,10 +44,20 @@ import { thunk } from "redux-thunk";
 import { authReducer } from "./authentication/Reducer";
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import restaurantReducer from "./restaurant/Reducer";
+import menuItemReducer from "./menu/Reducer";
+import cartReducer from "./cart/Reducer";
+import { orderReducer } from "./order/Reducer";
+import restaurantsOrderReducer from "./restaurant Order/Reducer";
+import { ingredientReducer } from "./ingredients/Reducer";
 
 const rootReducer = combineReducers({
     auth: authReducer,
-    restaurant:restaurantReducer
+    restaurant:restaurantReducer,
+    menu:menuItemReducer,
+    cart:cartReducer,
+    order:orderReducer,
+    restaurantOrder: restaurantsOrderReducer,
+    ingredients: ingredientReducer
 });
 
 // Load user & token from localStorage
---
+++ Frontend/src/components/state/order/Reducer.js
@@ -0,0 +1,22 @@
+import { GET_USER_FAILURE } from "../authentication/Action";
+import { GET_USERS_ORDERS_FAILURE } from "./ActionTypes";
+
+const initialState ={
+    loading:false,
+    orders:[],
+    error:null,
+    
+};
+
+export const orderReducer = (state = initialState, { type, payload }) => { 
+    switch (type) { 
+    case GET_USERS_ORDERS_REQUEST: 
+        return {...state, error: null, loading: true }; 
+    case GET_USERS_ORDERS_SUCCESS: 
+        return {...state, error: null, loading: false, orders: payload }; 
+    case GET_USERS_ORDERS_FAILURE: 
+        return {...state, error: payload, loading: false }; 
+    default: 
+        return state; 
+    } 
+};
